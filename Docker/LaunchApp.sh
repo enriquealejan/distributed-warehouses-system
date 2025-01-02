fb9998740f258d7d165e42c9fad66a3af796f84f
chuwa-tech <chuwakapj@gmail.com>
2025-01-02 11:36:08 +0100

    LaunchApp for Linux update

--- Docker/LaunchApp.sh
+++ Docker/LaunchApp.sh
@@ -3,11 +3,15 @@
 # Asegurarnos de descargar la imagen de mvn para compilar el proyecto
 docker pull maven:latest # La versión se puede cambiar
 
+cd .. # Me voy a la carpeta raíz del proyecto
+
 # Lanzar el contenedor de maven 
-docker run -it --rm --name project_compiler -v ../Gestion_Almacenes/:/app -w /app maven:latest mvn clean package # -v <source> lo cambias en función de donde se tenga el proyecto
+docker run -it --rm --name project_compiler -v Gestion_Almacenes/:/app -w /app maven:latest mvn clean package # -v <source> lo cambias en función de donde se tenga el proyecto
+
+cd Docker # Vuelvo a la carpeta de Docker
 
 # El .jar generado lo copio en el directorio de app (evitar posibles problemas)
-cp ../Gestion_Almacenes/target/Gestion_Almacenes-1.0-SNAPSHOT.jar "$(pwd)"/app
+cp ../Gestion_Almacenes/target/Gestion_Almacenes-1.0-SNAPSHOT.jar app
 
 # Una vez compilado el proyecto, creo la red para que mis contenedores interactuen
 docker network create app-network
